16ca0a80ad1b1e784ab9dcc885f9d531a4fa3372
pawanshettyy <pawanshetty13092006@gmail.com>
2024-10-25 19:44:48 +0530

    Made Critical Components

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';;
+import PopularProducts from './PopularProducts';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Services from './Services';
+import SpecialOffers from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+export{
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+}
