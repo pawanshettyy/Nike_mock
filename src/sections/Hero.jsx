55943402b2edc3b06737f76a699187fdf70ba699
pawanshettyy <pawanshetty13092006@gmail.com>
2024-10-26 21:53:55 +0530

    Hero Section in working

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,36 @@
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+import { statistics } from "../"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section 
+    id="home"
+    className="w-full
+    xl:flex-row 
+    flex-col 
+    justify-center 
+    min-h-screen 
+    gap-10 
+    max-container ">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+          <p>Our Summer Collection</p>
+          <h1>
+            <span>
+              The New Arrival
+            </span>
+            <br />
+            <span>Nike</span>
+          </h1>
+          <p1>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p1>
+          <Button label="Shop Now"
+          iconUrl={arrowRight}/>
+
+          <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+            {statistics}
+          </div>
+      </div>
+    </section>
   )
 }
 
