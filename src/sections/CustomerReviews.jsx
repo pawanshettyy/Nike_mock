9c3682b627ec002913f424ae7de4bb720929d862
pawanshettyy <pawanshetty13092006@gmail.com>
2024-10-29 21:48:15 +0530

    Fixed Bug - Customer Review bg-color

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -3,28 +3,28 @@ import { reviews } from "../constants";
 
 const CustomerReviews = () => {
   return (
-    <section className='max-container bg-black text-white'>
-      <h3 className='font-palanquin text-center text-4xl font-bold'>
-        What Our
-        <span className='text-coral-red'> Customers </span>
-        Say?
-      </h3>
-      <p className='m-auto mt-4 max-w-lg text-center info-text'>
-        Discover heartfelt testimonials from our loyal customers about the outstanding service and memorable experiences they've had with us.
-      </p>
+    <div className="w-full bg-black p-0 m-0">
+      <section className="max-container text-white py-16">
+        <h3 className="font-palanquin text-center text-4xl font-bold">
+          What Our <span className="text-coral-red">Customers</span> Say?
+        </h3>
+        <p className="m-auto mt-4 max-w-lg text-center info-text">
+          Discover heartfelt testimonials from our loyal customers about the outstanding service and memorable experiences they've had with us.
+        </p>
 
-      <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'>
-        {reviews.map((review, index) => (
-          <ReviewCard
-            key={index}
-            imgURL={review.imgURL}
-            customerName={review.customerName}
-            rating={review.rating}
-            feedback={review.feedback}
-          />
-        ))}
-      </div>
-    </section>
+        <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+          {reviews.map((review, index) => (
+            <ReviewCard
+              key={index}
+              imgURL={review.imgURL}
+              customerName={review.customerName}
+              rating={review.rating}
+              feedback={review.feedback}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
   );
 };
 
